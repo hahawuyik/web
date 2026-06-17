a22071d0e8c303e0d017470166853853a0d38699
吴艳（Wu Yan） <3521747315@qq.com>
2026-06-17 16:16:25 +0800

    增加请求超时时间至60秒

--- frontend-user/src/utils/request.js
+++ frontend-user/src/utils/request.js
@@ -4,7 +4,7 @@ import router from '@/router'
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL ,
-  timeout: 30000
+  timeout: 60000
 })
 
 request.interceptors.request.use(
